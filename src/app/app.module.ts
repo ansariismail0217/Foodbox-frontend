43dc2dc5416baa6d9fa230496c5265908bb15c0e
Abdullah Ismail Ansari <ansariismail0217@gmail.com>
2022-06-14 12:21:13 +0530

    release-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+import { RatingModule } from 'ng-starrating';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RatingModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/common/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+
+    name!: string;
+    cookTime!: string;
+    unitPrice!: number;
+    imageUrl!:string;
+    rating!: number;
+
+}
